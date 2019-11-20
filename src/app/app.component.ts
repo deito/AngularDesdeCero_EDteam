050d1106ec6d451b8c2d0f52d3f7bc6f94918f1b
deito <deito.alvarez@gmail.com>
2019-11-20 15:54:28 -0500

    3.5 - Función de plantillas en componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ed-root', // <ed-root></ed-root>
-  template: `
-    <h1>Curso de Angular</h1>
-    <span class="titulo fuente">EDteam</span>
-  `,
+  templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
+  constructor(){
+    console.log('Creando Objeto: AppComponent');
+  }
   title = 'edteam-curso-angular';
 }
