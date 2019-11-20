d4ff08acc77d96047b630e362023490e0ab11f68
deito <deito.alvarez@gmail.com>
2019-11-20 15:27:58 -0500

    3.4 - Definiendo estilos: In line

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ed-root', // <ed-root></ed-root>
   template: `
-    <ed-navbar></ed-navbar>
-    <ed-sidenav></ed-sidenav>
-    <ed-dashboard></ed-dashboard>
+    <h1>Curso de Angular</h1>
+    <span class="titulo fuente">EDteam</span>
   `,
-  styleUrls: ['./app.component.css']
+  styles: [
+    'h1 {font-weight: bold}',
+    'h1 {font-size: 10px}',
+    '.titulo {color: red;}',
+    '.fuente {font-size: 20px}'
+  ]
 })
 export class AppComponent {
   title = 'edteam-curso-angular';
