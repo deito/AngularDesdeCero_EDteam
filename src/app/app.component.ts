7095d737cbd6dbc349abc9761d84954f15a2d8ae
deito <deito.alvarez@gmail.com>
2019-11-20 15:12:36 -0500

    3.3 - Implementando un demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ed-root',
-  templateUrl: './app.component.html',
+  selector: 'ed-root', // <ed-root></ed-root>
+  template: `
+    <ed-navbar></ed-navbar>
+    <ed-sidenav></ed-sidenav>
+    <ed-dashboard></ed-dashboard>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
