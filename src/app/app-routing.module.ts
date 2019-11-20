7b3a24bb4afaa6820715195210459b35bb73f504
deito <deito.alvarez@gmail.com>
2019-11-20 17:25:54 -0500

    from '3.6 - Angular router' to '4.2 - Interpolación (1ra parte)'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { CoursesComponent } from './courses/courses.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard', // localhost:4200/dashboard
+    component: DashboardComponent
+  },
+  {
+    path: 'navbar', // localhost:4200/navbar
+    component: NavbarComponent
+  },
+  {
+    path: 'sidenav', // localhost:4200/sidenav
+    component: SidenavComponent
+  },
+  {
+    path: 'courses',
+    component: CoursesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
